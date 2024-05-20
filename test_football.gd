1d5889e9ae2e73f7023561f65a1c85cd562fbebb
xtempore <jbriden@gmail.com>
2024-05-21 11:00:57 +1200

    quick

--- test_football.gd
+++ test_football.gd
@@ -10,12 +10,16 @@ func _process(delta):
 	var change_strength = Input.get_axis("ui_down", "ui_up");
 	kick_strength = clamp(kick_strength + change_strength * 50 * delta, 2, 150)
 	$StrengthLabel.text = str(round(kick_strength))
-	print($Ball.global_transform.origin.z)
 
 func _input(event):
 	if event.is_action_pressed("ui_kick"):
 		$Leg/Foot.apply_central_impulse(Vector3(0, 0, -kick_strength))
 	elif event.is_action_pressed("ui_reset"):
-		print('reset')
-		$Ball.global_transform.origin = ball_pos  # Reset position
-	
+		reset_ball()
+		
+func reset_ball():
+	$Ball.global_transform.origin = ball_pos  # Reset position
+	$Ball.linear_velocity = Vector3.ZERO	
+	$Ball.angular_velocity = Vector3.ZERO
+	$Leg/Foot.linear_velocity = Vector3.ZERO
+	$Leg/Foot.angular_velocity = Vector3.ZERO
